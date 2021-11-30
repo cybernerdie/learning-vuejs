a6c535ea9feadf9c30e23583fa075649fd058fc7
Joshua Paul <veecthorpaul@gmail.com>
2021-11-30 15:18:38 +0100

    task tracker

---
+++ task-tracker/src/router/index.js
@@ -0,0 +1,24 @@
+import { processExpression } from '@vue/compiler-core'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home'
+import About from '../views/About'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL), routes
+})
+
+export default router
