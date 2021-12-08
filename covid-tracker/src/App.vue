6fa23457f16df34dd2e023fd9cc6de21579035b5
Joshua Paul <veecthorpaul@gmail.com>
2021-12-08 11:24:11 +0100

    covid tracker

--- covid-tracker/src/App.vue
+++ covid-tracker/src/App.vue
@@ -3,14 +3,17 @@
   <div class="container">
     <router-view />
   </div>
+ <Footer />
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
---
+++ covid-tracker/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-center bg-red-800 text-white p-4 mt-20">
+    <p>
+  Built with  <i class="fa fa-heart text-red-500"></i> 
+      <a class="text-white-300" href="http://joshuapaul.herokuapp.com/" target="_blank"
+      > Joshua Paul</a
+      >
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+sty
